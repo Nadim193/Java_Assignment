95f0e44dd5e98779e98c4335900d0ab54011a3a3
Nadim193 <nadim.hossain.nh18.nh.nh@gmail.com>
2020-02-15 00:03:18 +0600

    ADDRESS MAIN FUNCTION

---
+++ Addresstest.java
@@ -0,0 +1,11 @@
+
+package assignment;
+
+public class Addresstest {
+    public static void main(String[] args) {
+                    Address ob = new Address(317,1720,532 ,"JOYDEBPUR ","GAZIPUR ","BANGLADESH ");
+                    
+                    ob.print();
+                    ob.compared();
+    }
+}
