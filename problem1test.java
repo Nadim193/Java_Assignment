4a2509bf940cc57daea54a6d2e942ff8aa7d7eef
Nadim193 <nadim.hossain.nh18.nh.nh@gmail.com>
2020-02-15 00:00:01 +0600

    PROBLEM1 MAIN FUNCTION

---
+++ problem1test.java
@@ -0,0 +1,16 @@
+
+package assignment;
+
+public class problem1test {
+    
+    public static void main(String[] args) {
+
+                    problem1 ob = new problem1(5);
+                    ob.f();
+                    ob.g();
+                    ob.h();
+                    ob.k();
+                    
+    }
+    
+}
